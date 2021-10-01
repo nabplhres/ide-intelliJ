f5d941ac91b65e0f068219cade22c1f13de0faab
nabplhres <nabila.palhares@ee.ufcg.edu.br>
2021-09-30 21:59:03 -0300

    FIRST COMMIT

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,44 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Livro livro1 = new Livro(" orgulho e  preconceito ", 300);
+        System.out.println(livro1);
+
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
